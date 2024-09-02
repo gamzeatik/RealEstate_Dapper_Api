9d05a30cf4bf36a942312cb4b1302f9302dac3c5
gamzeatik <gamzeatik33@hotmail.com>
2024-09-02 23:53:44 +0300

    fixed the path bug

--- RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -1,6 +1,6 @@
 ﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
 
-namespace RealEstate_Dapper_Api.Repositories
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
 {
     public interface ICategoryRepository
     {
