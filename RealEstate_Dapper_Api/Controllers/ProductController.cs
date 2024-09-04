589641e7680398e0c8d46ed7cf17a65f32ab31bc
gamzeatik <gamzeatik33@hotmail.com>
2024-09-04 13:22:18 +0300

    product apis

---
+++ RealEstate_Dapper_Api/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.ProductRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+            var values = await _repository.GetAllProductsAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("GetAllProductsWithCategoryAsync")]
+        public async Task<IActionResult> GetAllProductsWithCategoryAsync()
+        {
+            var values = await _repository.GetAllProductsWithCategoryAsync();
+            return Ok(values);
+        }
+    }
+}
